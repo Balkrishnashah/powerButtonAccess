10f94b834c913f231c78ab351630295cf1a278c7
Balkrishnashah <shahkrishna493@gmail.com>
2020-12-07 10:46:12 +0530

    Initial commit

---
+++ app/src/main/java/com/balkrishnashah/powerbutton/MainActivity.java
@@ -0,0 +1,41 @@
+package com.balkrishnashah.powerbutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ScreenOnOffReceiver screenOnOffReceiver = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Alexcrue - Keep BroadcastReceiver Running After App Exit.");
+
+        // Create an IntentFilter instance.
+        IntentFilter intentFilter = new IntentFilter();
+
+        // Add network connectivity change action.
+        intentFilter.addAction("android.intent.action.SCREEN_ON");
+        intentFilter.addAction("android.intent.action.SCREEN_OFF");
+
+        // Set broadcast receiver priority.
+//        intentFilter.setPriority(100);
+
+        // Create a network change broadcast receiver.
+//        screenOnOffReceiver = new ScreenOnOffReceiver();
+
+        Intent backgroundService = new Intent(getApplicationContext(), ScreenOnOffBackgroundService.class);
+        startService(backgroundService);
+
+        // Register the broadcast receiver with the intent filter object.
+        registerReceiver(screenOnOffReceiver, intentFilter);
+
+        Log.d(ScreenOnOffReceiver.SCREEN_TOGGLE_TAG, "onCreate: screenOnOffReceiver is registered.");
+
+    }
+}
